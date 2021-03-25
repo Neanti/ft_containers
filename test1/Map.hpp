7d39cbf41858687e2ceeffee0a9bfb05260b80b5
Audric GAY <augay@e1r10p22.42.fr>
2021-03-25 11:29:34 +0100

    mapstart

--- test1/Map.hpp
+++ test1/Map.hpp
@@ -27,11 +27,64 @@ namespace ft {
 					return;
 				}
 
+				NodeMap *getnext()
+				{
+					NodeMap* next(this);
+					if (next->right)
+					{
+						next = next->right;
+						while(next->left)
+							next = next->left;
+					}
+					else {
+						NodeMap *tmp = next;
+						next = next->parent;
+						while (next->left != tmp)
+						{
+							tmp = next;
+							next = next->parent;
+						}
+					}
+					return next;
+				}
+
+				bool operator==(const NodeMap &other)
+				{
+					return value == other.value;
+				}
+
+				bool operator!=(const NodeMap &other)
+				{
+					return value != other.value;
+				}
+				bool operator<(const NodeMap &other)
+				{
+					return value < other.value;
+				}
+				bool operator>(const NodeMap &other)
+				{
+					return value > other.value;
+				}
+				bool operator<=(const NodeMap &other)
+				{
+					return value <= other.value;
+				}
+				bool operator>=(const NodeMap &other)
+				{
+					return value >= other.value;
+				}
+
 			};
 
 	template < class Key, class T, class Compare = less<Key>, class Alloc = allocator<pair<const Key,T> > >
 	class Map{
+	private:
 		typedef std::pair<const Key, T> value_type;
+		NodeMap<value_type, Compare> *root;
+		Compare comp;
+		size_t lenght;
+	public:
+
 	};
 
 }
