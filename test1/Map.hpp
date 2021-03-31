3c344b9432c316cfc9c22bca8a9385fb4c2597bd
Audric GAY <augay@e2r10p2.42.fr>
2021-03-31 12:37:10 +0200

    mapt

--- test1/Map.hpp
+++ test1/Map.hpp
@@ -6,6 +6,322 @@
 
 namespace ft {
 
+	template<class Key, class T, class Compare = std::less<Key> >
+	class MapIterator {
+	protected:
+		N *p;
+	public:
+		MapIterator() : p(0) {
+			return;
+		}
+
+		MapIterator(N *p) : p(p) {
+			return;
+		}
+
+		MapIterator(const MapIterator<T, N> &it) {
+			p = it.p;
+			return;
+		}
+
+		T getData(void) const {
+			return this->p->data;
+		}
+
+		bool operator!=(const MapIterator<T, N> &it) {
+			if (this->p != it.p)
+				return (true);
+			return false;
+		}
+
+		const T &operator*() const {
+			return this->p->data;
+		}
+
+		MapIterator<T, N> &operator++() {
+			p = p->next;
+			return *this;
+		}
+
+		MapIterator<T, N> operator++(int) {
+			MapIterator<T, N> tmp = *this;
+			p = p->next;
+			return tmp;
+		}
+
+		MapIterator<T, N> &operator--() {
+			p = p->prev;
+			return *this;
+		}
+
+		MapIterator<T, N> operator--(int) {
+			MapIterator<T, N> tmp = *this;
+			p = p->prev;
+			return tmp;
+		}
+
+		void setp(N *p) {
+			this->p = p;
+			return;
+		}
+
+		N *getp() {
+			return p;
+		}
+
+		N *getp() const {
+			return p;
+		}
+
+	};
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	std::ostream &operator<<(std::ostream &output, const MapIterator<T, N> in) {
+		std::cout << in.getData() << std::endl;
+		return output;
+	}
+
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	class RevMapIterator {
+	protected:
+		N *p;
+	public:
+		RevMapIterator() : p(0) {
+			return;
+		}
+
+		RevMapIterator(N *p) : p(p) {
+			return;
+		}
+
+
+		RevMapIterator(const RevMapIterator<T, N> &it) {
+			p = it.p;
+			return;
+		}
+
+		T getData(void) const {
+			return this->p->data;
+		}
+
+		bool operator!=(const RevMapIterator<T, N> &it) {
+			if (this->p != it.p)
+				return (true);
+			return false;
+		}
+
+		const T &operator*() const {
+			return this->p->data;
+		}
+
+		RevMapIterator<T, N> &operator--() {
+			p = p->next;
+			return *this;
+		}
+
+		RevMapIterator<T, N> operator--(int) {
+			RevMapIterator<T, N> tmp = *this;
+			p = p->next;
+			return tmp;
+		}
+
+		RevMapIterator<T, N> &operator++() {
+			p = p->prev;
+			return *this;
+		}
+
+		RevMapIterator<T, N> operator++(int) {
+			RevMapIterator<T, N> tmp = *this;
+			p = p->prev;
+			return tmp;
+		}
+
+		void setp(N *p) {
+			this->p = p;
+			return;
+		}
+
+		N *getp() {
+			return p;
+		}
+
+		N *getp() const {
+			return p;
+		}
+
+	};
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	std::ostream &operator<<(std::ostream &output, const RevMapIterator<T, N> in) {
+		std::cout << in.getData() << std::endl;
+		return output;
+	}
+
+
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	class ConstMapIterator {
+	protected:
+		N *p;
+	public:
+		ConstMapIterator() : p(0) {
+			return;
+		}
+
+		ConstMapIterator(N *p) {
+			this->p = p;
+			return;
+		}
+
+		ConstMapIterator(const ConstMapIterator<T, N> &it) {
+			p = it.p;
+			return;
+		}
+
+		ConstMapIterator(const MapIterator<T, N> &it)
+		{
+			p = it.getp();
+		}
+
+		T getData(void) const {
+			return this->p->data;
+		}
+
+		bool operator!=(const ConstMapIterator<T, N> &it) {
+			if (this->p != it.p)
+				return (true);
+			return false;
+		}
+
+		const T &operator*() const {
+			return this->p->data;
+		}
+
+		ConstMapIterator<T, N> &operator++() {
+			p = p->next;
+			return *this;
+		}
+
+		ConstMapIterator<T, N> operator++(int) {
+			ConstMapIterator<T, N> tmp = *this;
+			p = p->next;
+			return tmp;
+		}
+
+		ConstMapIterator<T, N> &operator--() {
+			p = p->prev;
+			return *this;
+		}
+
+		ConstMapIterator<T, N> operator--(int) {
+			ConstMapIterator<T, N> tmp = *this;
+			p = p->prev;
+			return tmp;
+		}
+
+		void setp(N *p) {
+			this->p = p;
+			return;
+		}
+
+		N *getp() {
+			return p;
+		}
+
+		N *getp() const {
+			return p;
+		}
+
+	};
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	std::ostream &operator<<(std::ostream &output, const ConstMapIterator<T, N> in) {
+		std::cout << in.getData() << std::endl;
+		return output;
+	}
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	class ConstRevMapIterator {
+	protected:
+		N *p;
+	public:
+		ConstRevMapIterator() : p(0) {
+			return;
+		}
+
+		ConstRevMapIterator(N *p) : p(p) {
+			return;
+		}
+
+
+		ConstRevMapIterator(const ConstRevMapIterator<T, N> &it) {
+			p = it.p;
+			return;
+		}
+
+		ConstRevMapIterator(const RevMapIterator<T, N> &it)
+		{
+			p = it.getp();
+		}
+
+		T getData(void) const {
+			return this->p->data;
+		}
+
+		bool operator!=(const ConstRevMapIterator<T, N> &it) {
+			if (this->p != it.p)
+				return (true);
+			return false;
+		}
+
+		const T &operator*() const {
+			return this->p->data;
+		}
+
+		ConstRevMapIterator<T, N> &operator--() {
+			p = p->next;
+			return *this;
+		}
+
+		ConstRevMapIterator<T, N> operator--(int) {
+			ConstRevMapIterator<T, N> tmp = *this;
+			p = p->next;
+			return tmp;
+		}
+
+		ConstRevMapIterator<T, N> &operator++() {
+			p = p->prev;
+			return *this;
+		}
+
+		ConstRevMapIterator<T, N> operator++(int) {
+			ConstRevMapIterator<T, N> tmp = *this;
+			p = p->prev;
+			return tmp;
+		}
+
+		void setp(N *p) {
+			this->p = p;
+			return;
+		}
+
+		N *getp() {
+			return p;
+		}
+
+		N *getp() const {
+			return p;
+		}
+
+	};
+
+	template<class Key, class T, class Compare = std::less<Key> >
+	std::ostream &operator<<(std::ostream &output, const ConstRevMapIterator<T, N> in) {
+		std::cout << in.getData() << std::endl;
+		return output;
+	}
+
 	enum color {
 		BLACK, RED
 	};
@@ -91,7 +407,7 @@ namespace ft {
 			root = 0;
 		}
 
-
+		map(iterator first, iterator last)
 
 	};
 
--- test1/main.cpp
+++ test1/main.cpp
@@ -13,17 +13,8 @@ bool iss(int i1, int i2)
 }
 
 int main() {
-	ft::Vector<int> *l = new ft::Vector<int>(4, 12);
-	ft::Vector<int>::iterator it = l->begin();
-	std::cout << *it << std::endl;
-	ft::Vector<int>::const_iterator ir = l->begin();
-	std::cout << *ir << std::endl;
-	ft::Vector<int>::reverse_iterator rit = l->rend();
-	rit--;
-	std::cout << *rit << std::endl;
-	ft::Vector<int>::const_reverse_iterator rir = l->rend();
-	rir--;
-	std::cout << *rir << std::endl;
+	ft::Map<int, int> *blb = new ft::Map<int, int>();
+
 
 	return 0;
 }
